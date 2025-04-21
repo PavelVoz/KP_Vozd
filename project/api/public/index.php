b5edbec07c7f48b4010b760b63f669d5b9160712
Pavel <167427572+PavelVoz@users.noreply.github.com>
2025-04-21 13:25:19 +0300

    Delete project directory

--- project/api/public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
-
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../src/routes.php';
-
-$app->run();
--- project/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['logged_in']);
-unset($_SESSION['user_id']);
-unset($_SESSION['role']);
-header("Location: login.php");
-exit();
-?>
